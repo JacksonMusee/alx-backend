506b78cf7e9f8f8a4bb2db9433460c73ce76dcf0
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-25 23:52:59 +0300

    Complete task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -17,5 +17,5 @@ def index_range(page: int, page_size: int) -> Tuple:
     Does as above
     '''
     start = (page - 1) * page_size
-    end = start + (page_size - 1)
+    end = start + page_size
     return (start, end)
