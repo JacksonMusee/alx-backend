0bcebe3769fd29e7e953ece6ba8be46da134e1eb
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-27 09:07:36 +0300

    Complete task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''
+Copy index_range from the previous task and the following class into your code
+
+Implement a method named get_page that takes two integer arguments page with
+default value 1 and page_size with default value 10.
+'''
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    '''
+    Does as above
+    '''
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert page > 0
+        assert page_size > 0
+
+        start, end = index_range(page, page_size)
+
+        return self.__dataset[start, end]
