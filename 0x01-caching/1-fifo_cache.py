ab5ebb7f53d504920fb73c726a4f788744faf6aa
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-27 16:33:39 +0300

    Complete task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""FIFO cache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO cache class
+    """
+
+    def __init__(self):
+        """Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Add items to cache
+        """
+        if not key and not item:
+            exit()
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_item = next(iter(self.cache_data))
+            self.cache_data.pop(first_item)
+            print(f"DISCARD: {first_item}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves a record from cache
+        """
+        if not key:
+            return None
+
+        return self.cache_data.get(key)
