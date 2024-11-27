5f04037bcaa0d6605286e64ea6e2b2ce1dcd7b92
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-27 09:15:55 +0300

    Complete task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,8 +40,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert int(page) > 0
-        assert int(page_size) > 0
+
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
 
